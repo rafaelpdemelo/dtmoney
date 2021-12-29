a13b4c68931588581b1f95826106dd7e98c4ee75
Rafael Pereira <rafaelpdemelo@gmail.com>
2021-12-29 20:27:33 -0300

    Finalização do Projeto dtmoney

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -6,7 +6,7 @@ import incomeImg from '../../assets/income.svg';
 import outcomeImg from '../../assets/outcome.svg';
 import closeImg from '../../assets/close.svg'
 import { FormEvent, useState } from 'react';
-import { api } from '../../services/api';
+import { useTransactions } from '../../hooks/useTransactions';
 
 
 
@@ -17,23 +17,30 @@ interface NewTransactionModalProps{
 
 export function NewTransactionModal({isOpen, onRequestClose}: NewTransactionModalProps){
 
-  const [title, setTitle] = useState('');
-  const [value, setValue] = useState(0);
-  const [category, setCategory] = useState('');
+  const { createTransaction } = useTransactions();
 
+
+
+  const [title, setTitle] = useState('');
+  const [amount, setAmount] = useState(0);
+  const [category, setCategory] = useState('');
   const [type, setType] = useState('deposit');
 
-  function handleCreateNewTransaction(event: FormEvent){
+  async function handleCreateNewTransaction(event: FormEvent){
     event.preventDefault();
 
-  const data = {
-    title,
-    value, 
-    category,
-    type,
-  };
-  
-  api.post('/transactions', data)
+    await createTransaction ({
+      title,
+      amount,
+      category,
+      type,
+    })
+
+    setTitle('');
+    setAmount(0);
+    setCategory('');
+    setType('deposit');
+    onRequestClose();
 
   }
 
@@ -64,8 +71,8 @@ export function NewTransactionModal({isOpen, onRequestClose}: NewTransactionModa
     <input 
       type="number"
       placeholder="Valor"
-      value={value}
-      onChange={event => setValue(Number(event.target.value))}
+      value={amount}
+      onChange={event => setAmount(Number(event.target.value))}
       />
 
 
